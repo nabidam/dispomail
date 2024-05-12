8571a75fd4ecc20fabad90b797025d748445d943
nabidam <navidabbaspoor@gmail.com>
2024-05-12 19:05:19 +0330

    watch functionality

--- TempMail.py
+++ TempMail.py
@@ -34,6 +34,10 @@ class TempMail:
         provider (str, optional): what provider to use for creating accounts. Defaults to "mail.tm".
     """
 
+    accounts = []
+    seen_messages = []
+    watch = False
+
     def __init__(self, provider="mail.tm") -> None:
         self.provider = provider
         self.session = requests.Session()
@@ -47,6 +51,10 @@ class TempMail:
         if self.provider == "mail.tm":
             logger.info("calling create_from_mail_tm method")
             accs = self.create_from_mail_tm(number_of_accounts)
+        else:
+            raise NotImplementedError(
+                f"Provider {self.provider} is not implemented yet."
+            )
 
         return accs
 
@@ -63,7 +71,7 @@ class TempMail:
             username = generate_random_string(10) + "@" + domain
             password = generate_random_string(10, all_letters=True)
 
-            data = {"address": username, "password": password}
+            data = {"address": username.lower(), "password": password}
             logger.info("creating account with the credentials: %s", data)
 
             res = self.session.post(create_endpoint, json=data)
@@ -91,4 +99,98 @@ class TempMail:
             json.dump(accs, file)
 
         logger.info("accounts details are saved in file: %s", file_name)
+        self.accounts = accs
         return accs
+
+    def watch_for_mails(self, interval=10):
+        if self.provider == "mail.tm":
+            logger.info("calling watch_for_mail_tm method")
+            return self.watch_for_mail_tm(interval)
+        else:
+            raise NotImplementedError(
+                f"Provider {self.provider} is not implemented yet."
+            )
+
+    def get_mail_tm_token(self, account):
+        assert isinstance(account, dict), "Account must be a dictionary"
+        assert len(account) >= 2, "Account dictionary must have at least two keys"
+
+        assert "address" in account, "Address key is missing in the Account"
+        assert "password" in account, "Password key is missing in the Account"
+
+        token_endpoint = self.base_url + "/token"
+        logger.info("requesting auth token for account: %s", account)
+        token_res = self.session.post(token_endpoint, json=account)
+        try:
+            token_res_json = token_res.json()
+            token = token_res_json["token"]
+            logger.info("token %s is generated for account: %s", token, account)
+            return token
+        except Exception as e:
+            logger.error("couldn't generate token for account: %s", account)
+            logger.error("error was: %s", repr(e))
+            return None
+
+    def get_mail_tm_message(self, message_id, token):
+        message_endpoint = self.base_url + "/messages/" + message_id
+        headers = {"Authorization": "Bearer " + token}
+        logger.info("fetching message id: %s", message_id)
+        message_res = self.session.get(message_endpoint, headers=headers)
+        message_res.raise_for_status()
+
+        message_res_json = message_res.json()
+        logger.info(
+            "fetch successfully message id: %s, with data: %s",
+            message_id,
+            message_res_json,
+        )
+        return message_res_json
+
+    def get_mail_tm_messages(self, token):
+        messages_endpoint = self.base_url + "/messages"
+        logger.info("requesting messages list for token: %s", token)
+        headers = {"Authorization": "Bearer " + token}
+        messages_list_res = self.session.get(messages_endpoint, headers=headers)
+        messages_list_res.raise_for_status()
+
+        messages_list_res_json = messages_list_res.json()
+
+        messages_list = [
+            msg
+            for i, msg in enumerate(messages_list_res_json["hydra:member"])
+            if messages_list_res_json["hydra:member"][i]["id"] not in self.seen_messages
+        ]
+
+        logger.info("checking for new messages...")
+        new_messages = []
+        for message in messages_list:
+            message_id = message["id"]
+            self.seen_messages.append(message_id)
+            new_messages.append(
+                self.get_mail_tm_message(message_id=message_id, token=token)
+            )
+        return new_messages
+
+    def watch_for_mail_tm(self, interval=10):
+        if len(self.accounts) == 0:
+            logger.error("no accounts found, run create() or add_account() first.")
+            raise Exception("no accounts found, run create() or add_account() first.")
+
+        logger.info("setting watch flag to True (starting watch process)")
+        self.watch = True
+        while self.watch:
+            for account in self.accounts:
+                mail_tm_token = self.get_mail_tm_token(account=account)
+
+                new_messages = self.get_mail_tm_messages(token=mail_tm_token)
+
+                data_to_yield = {"account": account, "new_messages": new_messages}
+                logger.info("yielding message data: %s", data_to_yield)
+                yield data_to_yield
+
+                logger.info("sleeping %ss before fetching next messages", interval)
+                sleep(interval)
+
+    def stop_watch_for_mails(self):
+        logger.info("setting watch flag to False (stopping watch process).")
+        self.watch = False
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from TempMail import TempMail
 
 tempmail = TempMail()
-accounts = tempmail.create(10)
+
+accounts = tempmail.create(1)
 
 print(accounts)
+
+watcher = tempmail.watch_for_mails()
+
+for data in watcher:
+    print(data)
